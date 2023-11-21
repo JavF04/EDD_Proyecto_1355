35f80642ecf66d41825aa399a5cb78dfde23db23
JavF04 <javisfloresgomez@gmail.com>
2023-11-21 11:50:52 -0600

    Se termina la logica del programa

--- development_(codes)/src/edd_proyecto/Main.java
+++ development_(codes)/src/edd_proyecto/Main.java
@@ -7,9 +7,9 @@ public class Main {
         Biblioteca biblioteca_obj = new Biblioteca();
         AdminArchivo admin = new AdminArchivo();
        //  biblioteca_obj.crear_df()
-        MenuUsuario menu_obj = new MenuUsuario(biblioteca_obj);
-        admin.crear_listas(biblioteca_obj);
+        MenuUsuario menu_obj = new MenuUsuario(biblioteca_obj, admin);
         admin.crear_listas(menu_obj);
+        admin.crear_listas(biblioteca_obj);
         JOptionPane.showMessageDialog(null,"Bienvenido a \"Bukku \" su biblioteca favorita.");
         menu_obj.inicioDeSesion();
         }
